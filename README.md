522aba0812139a2cd5d4903c61ab358f618514b7
Nandalal <s.nandalal35@gmail.com>
2025-02-17 02:32:16 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,44 @@
+# Angular 17 JWT Authentication & Authorization example with Rest API
+
+Build Angular 17 JWT Authentication & Authorization example with Rest Api, HttpOnly Cookie and JWT (including HttpInterceptor, Router & Form Validation).
+- JWT Authentication Flow for User Registration (Signup) & User Login
+- Project Structure with HttpInterceptor, Router
+- Way to implement HttpInterceptor
+- How to store JWT token in HttpOnly Cookie
+- Creating Login, Signup Components with Form Validation
+- Angular Components for accessing protected Resources
+- How to add a dynamic Navigation Bar to Angular App
+- Working with Browser Session Storage
+
+## Flow for User Registration and User Login
+For JWT – Token based Authentication with Rest API, we’re gonna call 2 endpoints:
+- POST `api/auth/signup` for User Registration
+- POST `api/auth/signin` for User Login
+- POST `api/auth/signout` for User Logout
+
+## Build an Angular 17 CRUD example App to consume Rest APIs, display, modify & search data.
+
+## Tutorial Application in that:
+
+Each Tutorial has id, title, description, published status.
+We can create, retrieve, update, delete Tutorials.
+There is a Search bar for finding Tutorials by title.
+
+## Define Routes for Angular 17 CRUD example
+
+There are 3 main routes:
+– /tutorials for tutorials-list component
+– /tutorials/:id for tutorial-details component
+– /add for add-tutorial component
+
+
+## Project setup
+```
+npm install
+```
+### Run
+```
+Run `ng serve --port 4300` for a dev server. 
+
+Navigate to `http://localhost:4300/`.
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BoardAdminComponent } from './board-admin/board-admin.component';
+import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
+import { BoardUserComponent } from './board-user/board-user.component';
+
+import { AddTutorialComponent } from './add-tutorial/add-tutorial.component';
+import { TutorialDetailsComponent } from './tutorial-details/tutorial-details.component';
+import { TutorialsListComponent } from './tutorials-list/tutorials-list.component';
+
+import { httpInterceptorProviders } from './_helpers/http.interceptor';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    ProfileComponent,
+    BoardAdminComponent,
+    BoardModeratorComponent,
+    BoardUserComponent,
+    AddTutorialComponent,
+    TutorialDetailsComponent,
+    TutorialsListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [httpInterceptorProviders],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
